5934c21397070b84e9a5f605fdc13bb7aa50571c
Fred_Shi <hshi163@aucklanduni.ac.nz>
2024-01-07 21:33:11 +1300

    Create and test download_data.py

---
+++ download_data.py
@@ -0,0 +1,46 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+import pandas as pd
+
+def add_columns(df, name, ticker_symbol):
+    """
+    To add below columns in dataframe provided:
+        1. Download time
+        2. RunId (Eg: 2024-01-07 is 240107)
+        3. Intraday_Id
+        4. ticker
+        5. Name
+    """
+    # Add columns
+    df['Download_time'] = datetime.now()
+    df['RunId'] = datetime.today().strftime("%y%m%d")
+    df['Intraday_Id'] = range(1, df.shape[0] + 1)
+    df['Ticker'] = ticker_symbol
+    df['Name'] = name
+    
+
+def download_price_data(name, ticker_symbol, start_date, end_date, interval):
+    """ 
+    Download historical prices for a certain ticker symbol in time of interest, with designated interval and add download time and ID column.
+    """
+    # 1. Decode interval
+    interval = '1wk' if interval == 'w' else '1d' if interval == 'd' else interval
+    # 2. Decode date (make end_date 1 day older)
+    end_date = datetime.strptime(end_date, "%Y-%m-%d") + timedelta(days = 1)
+    # 3. Download
+    df = yf.download(ticker_symbol, start=start_date,
+                       end=end_date, interval=interval)
+    # 4. Add columns
+    add_columns(df, name, ticker_symbol)
+    return df
+
+def download_data(names, start_date, end_date, interval):
+    """
+    To download price data for a dictionary of product names, append the result into a dataframe
+    """
+    df_result=pd.DataFrame()
+    for name in names:
+        df = download_price_data(name, names[name], start_date, end_date, interval)
+        df_result = df_result._append(df, ignore_index = True)
+    return df_result
+
---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+name_mapping = {
+    "Soybean Meal": "ZS=F",
+    "Corn": "ZC=F",
+    "Soybean Oil": "ZL=F",
+    "Lean Hogs": "HE=F",
+    "Live Cattle": "LE=F",
+    "Cocoa": "CC=F",
+    "Coffee": "KC=F",
+    "Cotton": "CT=F",
+    "Orange Juice": "OJ=F",
+    "Sugar": "SB=F"
+}
